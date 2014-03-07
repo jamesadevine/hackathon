19332b061ea61d46db02f8397279f01498656677
gunmania0 <james@devine.eu>
2014-03-07 01:03:28 +0000

    asdasdasd

--- app/src/main/java/com/nullpointerexception/hackathon/app/GoogleCloudManager.java
+++ app/src/main/java/com/nullpointerexception/hackathon/app/GoogleCloudManager.java
@@ -1,10 +1,5 @@
 package com.nullpointerexception.hackathon.app;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.util.Log;
-
 /**
  * Created by James on 07/03/2014.
  */
@@ -14,29 +9,5 @@ public class GoogleCloudManager {
     public GoogleCloudManager(){
 
     }
-
-    private String getRegistrationId(Context context,Activity activity) {
-        final SharedPreferences prefs = getGCMPreferences(context);
-        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
-        if (registrationId.isEmpty()) {
-            Log.i(TAG, "Registration not found.");
-            return "";
-        }
-        // Check if app was updated; if so, it must clear the registration ID
-        // since the existing regID is not guaranteed to work with the new
-        // app version.
-        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
-        int currentVersion = activity.getAppVersion(context);
-        if (registeredVersion != currentVersion) {
-            Log.i(TAG, "App version changed.");
-            return "";
-        }
-        return registrationId;
-    }
-    private SharedPreferences getGCMPreferences(Context context) {
-        // This sample app persists the registration ID in shared preferences, but
-        // how you store the regID in your app is up to you.
-        return getSharedPreferences(MainActivity.class.getSimpleName(),
-                Context.MODE_PRIVATE);
-    }
+    
 }
